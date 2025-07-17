cb1cf94c60c4e3e9ae85d25226de4dcde3ab5978
ali-ataeva <a.606777083@gmail.com>
2025-07-17 13:50:49 +0200

    feat(variables): Assign Elements to variables

--- src/handle_buttons.js
+++ src/handle_buttons.js
@@ -1,1 +1,23 @@
+let display = document.getElementById("display")
+
+let seven = document.getElementById("seven")
+let eight = document.getElementById("eight")
+let nine = document.getElementById("nine")
+let four = document.getElementById("four")
+let five = document.getElementById("five")
+let six = document.getElementById("six")
+let one = document.getElementById("one")
+let two = document.getElementById("two")
+let three = document.getElementById("three")
+let zero = document.getElementById("zero")
+let doubleZero = document.getElementById("double-zero")
+let comma = document.getElementById("comma")
+let percent = document.getElementById("percent")
+let AC = document.getElementById("AC")
+let multiply = document.getElementById("multiply")
+let divide = document.getElementById("divide")
+let add = document.getElementById("add")
+let subtract = document.getElementById("subtract")
+let equals = document.getElementById("equals")
+
 
